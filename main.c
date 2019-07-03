84ab183e65982c9c72f15a1822af11bdd20c20fc
avarsaje <52372844+avarsaje@users.noreply.github.com>
2019-07-03 07:27:41 +0430

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -65,189 +63,163 @@ struct node* create_node(int day,char name2[20],int start_time,int end_time)
 }
 
 
-
-void print_list(struct node* list)
-{
-    struct node* temp1=list;
-    while(temp1 !=NULL)
-    {
-        printf("%d %d %d %s\n",temp1->day,temp1->start_time,temp1->end_time,temp1->name1);
-        temp1=temp1->next;
-    }
-}void show(struct node* list){
-    struct node* temp1=list;
+void show(struct node* list){
+    struct node* current=list;
     int c[7]={0},cc[7]={0};
-    if(temp1==NULL)
+    if(current==NULL)
                 printf("nuuuuull");
-                while(temp1!= NULL)
+                while(current!= NULL)
                 {
-                	if(temp1->day==0)
+                	if(current->day==0)
                 	c[0]++;
-                	if(temp1->day==1)
+                	if(current->day==1)
                 	c[1]++;
-                	if(temp1->day==2)
+                	if(current->day==2)
                 	c[2]++;
-                	if(temp1->day==3)
+                	if(current->day==3)
                 	c[3]++;
-                	if(temp1->day==4)
+                	if(current->day==4)
                 	c[4]++;
-                	if(temp1->day==5)
+                	if(current->day==5)
                 	c[5]++;
-                	if(temp1->day==6)
+                	if(current->day==6)
                 	c[6]++;
-                	temp1=temp1->next;
+                	current=current->next;
 				}
-				printf("%d",c[0]);
-			temp1=list;
+			current=list;
 			printf("\n0 (Saturday):");
-                while(temp1!=NULL)
+                while(current!=NULL)
             {
 
-            	if(temp1->day==0)
+            	if(current->day==0)
 				{
-                	printf("%s (from: %d to: %d)",temp1->name1,temp1->start_time,temp1->end_time);
+                	printf("%s (from: %d to: %d)",current->name1,current->start_time,current->end_time);
                 	if(cc[0]<c[0]-1)
                 	printf("->");
                 	cc[0]++;
 
 				}
 
-                temp1=temp1->next;
+                current=current->next;
             }
-            	temp1=list;
+            	current=list;
 			printf("\n1 (Sunday):");
-                while(temp1!=NULL)
+                while(current!=NULL)
             {
 
-            	if(temp1->day==1)
+            	if(current->day==1)
 				{
-                	printf("%s (from: %d to: %d)",temp1->name1,temp1->start_time,temp1->end_time);
+                	printf("%s (from: %d to: %d)",current->name1,current->start_time,current->end_time);
                 	if(cc[1]<c[1]-1)
                 	printf("->");
                 	cc[1]++;
 				}
 
-                temp1=temp1->next;
+                current=current->next;
             }
-            	temp1=list;
+            	current=list;
 			printf("\n2 (Monday):");
-                while(temp1!=NULL)
+                while(current!=NULL)
             {
 
-            	if(temp1->day==2)
+            	if(current->day==2)
 				{
-                	printf("%s (from: %d to: %d)",temp1->name1,temp1->start_time,temp1->end_time);
+                	printf("%s (from: %d to: %d)",current->name1,current->start_time,current->end_time);
                 	if(cc[2]<c[2]-1)
                 	printf("->");
                 	cc[2]++;
 				}
 
-                temp1=temp1->next;
+                current=current->next;
             }
-            	temp1=list;
+            	current=list;
 			printf("\n3 (Tuesday):");
-                while(temp1!=NULL)
+                while(current!=NULL)
             {
 
-            	if(temp1->day==3)
+            	if(current->day==3)
 				{
-                	printf("%s (from: %d to: %d)",temp1->name1,temp1->start_time,temp1->end_time);
+                	printf("%s (from: %d to: %d)",current->name1,current->start_time,current->end_time);
                 	if(cc[3]<c[3]-1)
                 	printf("->");
                 	cc[3]++;
 				}
 
-                temp1=temp1->next;
+                current=current->next;
             }
-            	temp1=list;
+            	current=list;
 			printf("\n4 (Wednesday):");
-                while(temp1!=NULL)
+                while(current!=NULL)
             {
 
-            	if(temp1->day==4)
+            	if(current->day==4)
 				{
-                	printf("%s (from: %d to: %d)",temp1->name1,temp1->start_time,temp1->end_time);
+                	printf("%s (from: %d to: %d)",current->name1,current->start_time,current->end_time);
                 	if(cc[4]<c[4]-1)
                 	printf("->");
                 	cc[4]++;
 				}
 
-                temp1=temp1->next;
+                current=current->next;
             }
-            	temp1=list;
+            	current=list;
 			printf("\n5 (Thursday):");
-                while(temp1!=NULL)
+                while(current!=NULL)
             {
 
-            	if(temp1->day==5)
+            	if(current->day==5)
 				{
-                	printf("%s (from: %d to: %d)",temp1->name1,temp1->start_time,temp1->end_time);
+                	printf("%s (from: %d to: %d)",current->name1,current->start_time,current->end_time);
                 	if(cc[5]<c[5]-1)
                 	printf("->");
                 	cc[5]++;
 				}
 
-                temp1=temp1->next;
+                current=current->next;
             }
-            	temp1=list;
+            	current=list;
 			printf("\n6 (Friday):");
-                while(temp1!=NULL)
+                while(current!=NULL)
             {
 
-            	if(temp1->day==6)
+            	if(current->day==6)
 				{
-                	printf("%s (from: %d to: %d)",temp1->name1,temp1->start_time,temp1->end_time);
+                	printf("%s (from: %d to: %d)",current->name1,current->start_time,current->end_time);
                 	if(cc[6]<c[6]-1)
                 	printf("->");
                 	cc[6]++;
 				}
 
-                temp1=temp1->next;
+                current=current->next;
             }
             printf("\n");
 
-
-
     }
-
-
-
 
 int main()
 {
-   int start_time,end_time,day,i=0,lines=0,c[7]={0},cc[7]={0},flagg=0;
+   int start_time,end_time,day,i=0,lines=0,flagg=0;
    struct node* list=NULL;
-   struct node* temp1=NULL;
-   struct node temp2;
-   char name[30],name1[20];
+   struct node* current=NULL;
+   char name[30],name1[20],ch;
    int day1,start_time1,end_time1;
-   char ch1,name11[20];
+   char name11[20];
    printf("Enter your name:\n");
-   FILE *fp,*fpp;
+   FILE *fp;
 
    scanf("%s",name);
-   printf("Welcome %s\n\n\n",name);
-   int ch,size,flag=0;
+   printf("\nWelcome %s\n\n\n",name);
    fp=fopen(name,"a+");
    if(NULL==fp)
-    printf("File not found");
-   if (NULL != fp) {
-    fseek(fp, 0, SEEK_END);
-    size = ftell(fp);
-    if (0 == size) {
-    flag=1;
-    }
-    else
-    {
+    printf("Cant open the file");
 
-    }
-    }
    while(1)
     {   print_menu();
-        scanf("%d",&ch);
+        scanf("%c",&ch);
+        scanf("%c",&ch);
         switch(ch)
         {
-           case 1:
+           case '1':
                flagg=0;
                printf("Enter the day: \n");
                printf(">");
@@ -265,37 +237,38 @@ int main()
                printf(">");
                scanf("%d",&end_time);
                printf("\n");
-               temp1=list;
-               while(temp1!= NULL)
+               current=list;
+               while(current!= NULL)
                {
-               	if(temp1->day==day && strcmp(temp1->name1,name1)==0)
+               	if(current->day==day && strcmp(current->name1,name1)==0)
                	{
-               		printf("tekrari");
+               		printf("You have same activity in this day");
                		flagg=1;
                		break;
 				}
-				temp1=temp1->next;
+				current=current->next;
 			   }
-			   temp1=list;
-			   while(temp1!= NULL)
+			   current=list;
+			   while(current!= NULL)
                {
-               	if(temp1->day==day && temp1->start_time>start_time && temp1->start_time<end_time)
+               	if(current->day==day && current->start_time>start_time && current->start_time<end_time)
                	{
-               		printf("tadakhol");
+               		printf("Interference occurs\n");
                		flagg=1;
                		break;
 				}
-				temp1=temp1->next;
+				current=current->next;
 			   }
-			   while(temp1!= NULL)
+			   current=list;
+			   while(current!= NULL)
                {
-               	if(temp1->day==day && temp1->start_time<start_time && temp1->end_time>end_time)
+               	if(current->day==day && current->start_time<start_time && current->end_time>start_time)
                	{
-               		printf("tadakhol darad/n");
+               		printf("Interference occurs\n");
                		flagg=1;
                		break;
 				}
-				temp1=temp1->next;
+				current=current->next;
 			   }
                 if(flagg==1)
                     break;
@@ -312,27 +285,21 @@ int main()
                 list=add_front(list,temp);
                break;
 
-
-
-
-           case 2:
+           case '2':
                 show(list);
-               temp1=list;
-
-
                break;
-       
-           case 3:
-                   temp1=list;
-                while(temp1!=NULL)
+
+           case '3':
+                   current=list;
+                while(current!=NULL)
             {
-                fprintf(fp,"%d %d %d %s\n",temp1->day,temp1->start_time,temp1->end_time,temp1->name1);
-                temp1=temp1->next;
+                fprintf(fp,"%d %d %d %s\n",current->day,current->start_time,current->end_time,current->name1);
+                current=current->next;
             }
 
 
 			break;
-           case 4:
+           case '4':
                 fp=fopen(name,"r");
                 lines++;
                 while ((ch = fgetc(fp)) != EOF)
@@ -341,15 +308,6 @@ int main()
                         lines++;
                     }
                     fp=fopen(name,"r");
-                    if(lines==1)
-                     {
-                       fscanf(fp,"%d%d%d%s",&day1,&start_time1,&end_time1,name11);
-                       struct node* temp=create_node(day1,name11,start_time1,end_time1);
-                        list=add_end(list,temp);
-                        break;
-
-
-                    }
 
 
                  while(i<lines-1)
@@ -362,11 +320,13 @@ int main()
                     }
 
             break;
-           case 5:
+           case '5':
                fclose(fp);
                return 0;
+           case '\n':
+            break;
            default:
-           printf("wrong input");
+           printf("\nwrong input\n\n\n");
             break;
             }
     }
